3e0978290216f77900eb2f54ac8aa17075fc3433
Hamza Alayed <hamza.alayed@interiordefine.com>
2022-01-25 18:23:33 +0200

    add some more updates for setup script

--- setup.sh
+++ setup.sh
@@ -1,28 +1,41 @@
 #!/usr/bin/env bash
 
+PHP=php@8.1
+
 xcode-select --install
 
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+brew doctor
 
 brew update
 
 brew install wget
 
-brew install php@8.1
+if brew ls --versions $PHP > /dev/null; then
+  brew link --overwrite $PHP
+else
+  brew install $PHP
+fi
 
 brew install composer
 
+export PATH=$PATH:~/.composer/vendor/bin
+
 brew install dbngin
 
-brew install yarn
+if brew ls --versions yarn > /dev/null; then
+  brew link --overwrite yarn
+else
+  brew install yarn
+fi
 
 composer global require laravel/valet --with-all-dependencies
 
 valet install
 
-valet use php@8.1
+valet use  $PHP
 
 brew cleanup
